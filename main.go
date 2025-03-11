68f7b28edeba6231ea6a6d3fe90e2b45d4afc6d1
Cainã Contarin <caina.contarin@vulcabras.com>
2025-03-11 17:10:57 -0300

    Adição de props no layout padrão

--- main.go
+++ main.go
@@ -17,12 +17,6 @@ func main() {
 	app.Get("/", func(c fiber.Ctx) error {
 		return lib.Render(c, pages.Hello("Cainã"))
 	})
-	app.Get("/jsonRFID", func(c fiber.Ctx) error {
-		c.Set("Content-Type", "application/json")
-		return c.SendString(`
-{"DateTime":"2025-02-27T14:30:00Z","Task":["Picking","Pack","Store"],"Orders":[{"OrderID":12345,"CustomerID":67890,"Products":[{"ProductID":54321,"Color":"Blue","Size":"M","WMSAddress":[{"Aisle":"A12","Level":2,"Bin":"B15","Quantity":10}]},{"ProductID":98765,"Color":"Red","Size":"L","WMSAddress":[{"Aisle":"A14","Level":1,"Bin":"B22","Quantity":5}]}]},{"OrderID":67890,"CustomerID":54321,"Products":[{"ProductID":11111,"Color":"Black","Size":"S","WMSAddress":[{"Aisle":"A10","Level":3,"Bin":"B05","Quantity":20}]}]}]}
-		`)
-	})
 
 	app.Listen("localhost:8080")
 }
